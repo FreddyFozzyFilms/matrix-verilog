9f27f09560ebdca1adee6ca48723764f4936cb32
Frederick <fred2016.pu@gmail.com>
2022-06-01 13:15:16 -0400

    got rid of trash for loops in verilog

---
+++ binary_matrix/nbyn/02_vec_mul_guide.v
@@ -0,0 +1,35 @@
+// Time scale and resolution for simulation
+`timescale 1ns / 1ps
+
+/* In this module we define matrix vector multiplication for 2 x 2 matrices over
+the scaler field of the dihedral group D_2 of order 2 (multiplication and addition mod 2)
+
+in D_2, 
+    multiplication corresponds to and
+    addition corresponds to xor
+
+ */
+module matrix_vec_mul (
+
+    // 2x2 matrix M
+    input wire A_0_0,
+    input wire A_0_1,
+    input wire A_1_0,
+    input wire A_1_1,
+
+    input wire v_0,
+    input wire v_1,
+
+    // u
+    output wire u_0,
+    output wire u_1
+
+    // u = M v
+    );
+
+    //|g| |a  b| |e|
+    //| |=|    | | |
+    //|h| |c  d| |f|
+    assign u_0 = (A_0_0 && v_0) ^ (A_0_1 && v_1);
+    assign u_1 = (A_1_0 && v_0) ^ (A_1_1 && v_1); 
+endmodule
---
+++ binary_matrix/nbyn/02_vec_mul_guide_tb.v
@@ -0,0 +1,56 @@
+// Simulation time scale and precision
+`timescale 1ns / 1ps
+
+module test();
+
+    // Internal signals
+    /* Signals to make connections to the UUT */
+    reg A_0_0, A_0_1, A_1_0, A_1_1, v_0, v_1;    // inputs to the UUT
+    wire u_0, u_1;         // output of the UUT
+
+    matrix_vec_mul uut (
+    .A_0_0(A_0_0), .A_0_1(A_0_1), 
+    .A_1_0(A_1_0), .A_1_1(A_1_1), 
+
+    .v_0(v_0), .v_1(v_1),
+    .u_0(u_0), .u_1(u_1)
+    );
+
+    initial begin
+        // Input signal initialization
+        A_0_0 = 0; 
+        A_0_1 = 0;
+        A_1_0 = 0;
+        A_1_1 = 0;
+        
+        v_0 = 0;
+        v_1 = 0;
+
+        // Waveform generation
+        $dumpfile("02_2x2_vec_mul_tb.vcd");  // Waveform file
+        $dumpvars(0, test);         // signals to plot (everything)
+
+        // We finish simulation at t=100
+        #320 $finish;
+    end
+
+    /* Generation of input patterns to simulate the UUT */
+    // a, b and c will got through all possible combinations
+    always
+        #5 A_0_0 = ~A_0_0;
+    always
+        #10 A_0_1 = ~A_0_1;
+    always
+        #20 A_1_0 = ~A_1_0;
+    always
+        #40 A_1_1 = ~A_1_1;
+
+    always
+        #80 v_0 = ~v_0;
+    always
+        #160 v_1 = ~v_1;
+    
+    initial begin
+        $monitor("%d %d %d %d %d %d %d %d", A_0_0, A_0_1, A_1_0, A_1_1, v_0, v_1, u_0, u_1, );
+    end
+endmodule // test
