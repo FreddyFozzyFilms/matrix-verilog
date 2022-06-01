dfc43bc7b5bed7b018b5b33ef16e01775010f881
Frederick <fred2016.pu@gmail.com>
2022-06-01 13:25:21 -0400

    added generator

---
+++ binary_matrix/nbyn/generator.py
@@ -0,0 +1,23 @@
+f = open("vec_mul.v", "w")
+
+n = 2
+
+f.write("module matrix_vec_mul ( \n")
+
+# declarations
+# declare matrix
+for i in range(n):
+    for j in range(n):
+        f.write(f"input wire A_{i}_{j},\n")
+# declare vector in
+for i in range(n):
+    f.write(f"input wire v_{i},\n")
+# declare vector out
+for i in range(n - 1):
+    f.write(f"output wire u_{i}, \n")
+f.write(f"output wire u_{n-1}\n")
+
+
+
+f.write("pigu\n")
+f.close()
