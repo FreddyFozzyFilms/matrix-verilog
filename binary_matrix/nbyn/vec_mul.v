e4d232f15fd4028e5fcab188d1e6cde5239a3815
FredyFozzyFilms <fred2016.pu@gmail.com>
2022-06-26 20:52:48 -0400

    fft lol

--- binary_matrix/nbyn/vec_mul.v
+++ binary_matrix/nbyn/vec_mul.v
@@ -2,24 +2,16 @@
 module matrix_vec_mul ( 
 input wire A_0_0,
 input wire A_1_0,
-input wire A_2_0,
 input wire A_0_1,
 input wire A_1_1,
-input wire A_2_1,
-input wire A_0_2,
-input wire A_1_2,
-input wire A_2_2,
 
 input wire v_0,
 input wire v_1,
-input wire v_2,
 
 output wire u_0, 
-output wire u_1, 
-output wire u_2
+output wire u_1
 );
 
-assign u_0 = (A_0_0 && v_0) ^ (A_1_0 && v_1) ^ (A_2_0 && v_2);
-assign u_1 = (A_0_1 && v_0) ^ (A_1_1 && v_1) ^ (A_2_1 && v_2);
-assign u_2 = (A_0_2 && v_0) ^ (A_1_2 && v_1) ^ (A_2_2 && v_2);
+assign u_0 = (A_0_0 && v_0) ^ (A_1_0 && v_1);
+assign u_1 = (A_0_1 && v_0) ^ (A_1_1 && v_1);
 endmodule
